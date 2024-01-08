edfd0b7cb83f78da50e9b2167f370573f81ed7ef
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-01-08 12:24:40 +0600

    search by book and author name in all books page

--- src/Pages/AllBooks/AllBooks.jsx
+++ src/Pages/AllBooks/AllBooks.jsx
@@ -6,6 +6,7 @@ const AllBooks = () => {
   const [filteredBooks, setFilteredBooks] = useState([]);
   const [showAvailableOnly, setShowAvailableOnly] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     fetch('https://boipoka-server.vercel.app/books')
@@ -20,13 +21,23 @@ const AllBooks = () => {
     setShowAvailableOnly(!showAvailableOnly);
   };
 
+  const handleSearch = () => {
+    setFilteredBooks(
+      allBooks.filter(
+        (book) =>
+          book.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          book.author.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+  };
+
   useEffect(() => {
     if (showAvailableOnly) {
       setFilteredBooks(allBooks.filter((book) => book.quantity > 0));
     } else {
-      setFilteredBooks(allBooks);
+      handleSearch();
     }
-  }, [showAvailableOnly, allBooks]);
+  }, [showAvailableOnly, allBooks, searchTerm]);
 
   return (
     <div className="flex justify-center items-center flex-col">
@@ -37,13 +48,27 @@ const AllBooks = () => {
           <h1 className="text-2xl font-semibold my-20 text-center">
             Popular books
           </h1>
-          <div className="mb-4">
-            <button
-              className={`btn ${showAvailableOnly ? 'btn-active' : ''}`}
-              onClick={handleFilterToggle}
-            >
-              {showAvailableOnly ? 'Show All Books' : 'Show Available Books'}
-            </button>
+          <div className="mb-4 flex items-center justify-between">
+            <div>
+              <button
+                className={`btn ${showAvailableOnly ? 'btn-active' : ''}`}
+                onClick={handleFilterToggle}
+              >
+                {showAvailableOnly ? 'Show All Books' : 'Show Available Books'}
+              </button>
+            </div>
+            <div className="flex items-center">
+              <input
+                type="text"
+                placeholder="Search by title or author"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                className="form-input mr-2"
+              />
+              <button className="btn" onClick={handleSearch}>
+                Search
+              </button>
+            </div>
           </div>
           <div className="grid md:grid-cols-3 gap-12">
             {filteredBooks.map((book) => (
--- src/Pages/Borrows/Borrows.jsx
+++ src/Pages/Borrows/Borrows.jsx
@@ -18,27 +18,6 @@ const Borrows = () => {
     axiosSecure.get(url).then((res) => setBorrows(res.data));
   }, [url, axiosSecure]);
 
-  // const handleReturn2 = (id) => {
-  //   fetch(`https://boipoka-server.vercel.app/borrows/${id}`, {
-  //     method: 'PATCH',
-  //     headers: {
-  //       'content-type': 'application/json',
-  //     },
-  //     body: JSON.stringify({ status: 'confirm' }),
-  //   })
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       console.log(data);
-  //       if (data.modifiedCount > 0) {
-  //         // update state
-  //         const remaining = borrows.filter((borrow) => borrow._id !== id);
-  //         const updated = borrows.find((borrow) => borrow._id === id);
-  //         updated.status = 'confirm';
-  //         const newBorrows = [updated, ...remaining];
-  //         setBorrows(newBorrows);
-  //       }
-  //     });
-  // };
 
   const handleReturn = (id) => {
     Swal.fire({
@@ -87,19 +66,11 @@ const Borrows = () => {
     });
   };
 
-  // if (updateData.success == true) {
-  //   Swal.fire({
-  //     title: 'Borrowed successfully!',
-  //     text: 'Do you want to continue?',
-  //     icon: 'success',
-  //     confirmButtonText: 'Sure!',
-  //   });
-  // }
 
   return (
     <div>
       <div className="overflow-x-auto w-full flex flex-col justify-center items-center">
-        <h1 className="text-2xl font-bold mt-16 mb-20">My Borrowed Books</h1>
+        <h1 className="text-2xl font-bold h-screen pt-28 dark:text-slate-50">My Borrowed Books</h1>
 
         <table className="table w-full">
           {/* head */}
